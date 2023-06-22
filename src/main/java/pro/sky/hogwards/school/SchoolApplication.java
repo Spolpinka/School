ad554e1ccfb54c851bdead7f0f5a740fa079eb14
Spolpinka <a421243266@gmail.com>
2023-06-22 10:28:56 +0300

    Initial commit

---
+++ src/main/java/pro/sky/hogwards/school/SchoolApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hogwards.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+
+}
